0fdac3dfa14b25def72ba5e35a952d81e30d12ec
Kisara10 <kisara.radev@gmail.com>
2026-04-21 22:10:01 +0530

    Setup Tomcat project with Jersey dependencies and working build

--- src/main/java/com/kisara/api/config/AppConfig.java
+++
@@ -1,9 +0,0 @@
-package com.kisara.api.config;
-
-import javax.ws.rs.ApplicationPath;
-import javax.ws.rs.core.Application;
-
-@ApplicationPath("/api/v1")
-
-public class AppConfig extends Application{
-}
